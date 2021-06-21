fc959e2469aa159fe0e3de5e2d43bee66a0ee829
fischer <imankermani@hotmail.com>
2021-06-21 20:35:49 +0430

    fixing workflows

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="jcalendar",
-    version="0.1.1",
+    version="0.1.2",
     author="Iman Kermani",
     author_email="imankermani97@outlook.com",
     description="Jalali calendar binding for python",
