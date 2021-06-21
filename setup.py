b969f2b71f762880bb24b2c1a8e3cc4faa9fe765
fischer <imankermani@hotmail.com>
2021-06-21 20:57:07 +0430

    fix versioning

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="jcalendar",
-    version="0.1.2",
+    version="0.1.3",
     author="Iman Kermani",
     author_email="imankermani97@outlook.com",
     description="Jalali calendar binding for python",
